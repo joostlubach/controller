752526b99fab2254f266d6186c1a68b105a790df
Joost Lubach <joostlubach@gmail.com>
2024-04-02 11:03:36 +0200

    Converted to ES Modules

--- src/decorators/use.ts
+++ src/decorators/use.ts
@@ -1,7 +1,7 @@
 import { Constructor, TypedClassDecorator } from 'ytil'
 
-import registry from '../registry'
-import { MiddlewareFunction, MiddlewareOptions } from '../types'
+import registry from '../registry.js'
+import { MiddlewareFunction, MiddlewareOptions } from '../types.js'
 
 export function use<C extends Constructor<any>>(func: MiddlewareFunction<InstanceType<C>>, options: MiddlewareOptions = {}): TypedClassDecorator<C> {
   return Controller => {
